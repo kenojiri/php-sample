49dd1e54daf69a2e6040ea0fbf7dec951d574ac8
nttlab-ojiri <nttlab-ojiri@nttr.co.jp>
2017-01-18 14:37:25 +0900

    first commit

---
+++ sample.php
@@ -0,0 +1,14 @@
+<?php
+class Sample{
+  protected $value;
+ 
+  public function get_value(){
+    return $this->value;
+    throw new RuntimeException('Not yet implemented.');
+  }
+  
+  public function set_value($value){
+    $this->value = $value * 2;
+  }
+} 
+?>
---
+++ sample_test.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'sample.php';
+ 
+class SampleTest extends PHPUnit_Framework_TestCase{
+ 
+  public function setUp(){
+    $this->sample = new Sample;
+  }
+ 
+  public function testGetValue(){
+    $this->sample->set_value(20);
+    $this->assertEquals(40,$this->sample->get_value());
+  }
+}
+?>
