b4443620c107b7fc2d13ed2eaf9fa2ddd0c01cbb
Ken Ojiri <ozzozz@users.noreply.github.com>
2017-03-02 20:39:44 +0900

    fix sample.php for phpunit and phpcs

--- sample.php
+++ sample.php
@@ -1,16 +1,59 @@
 <?php
+/**
+ * Sample File Doc Comment
+ *
+ * @category Class
+ * @package  sample
+ * @author    A N Other 
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ *
+ */
 namespace sample;
 
-class Sample{
-  protected $value;
- 
-  public function getValue(){
-    return $this->value;
-    throw new RuntimeException('Not yet implemented.');
-  }
-  
-  public function setValue($value){
-    $this->value = $value * 3;
-  }
+/**
+ * Sample Class Doc Comment
+ *
+ * @category Class
+ * @package  sample
+ * @author    A N Other 
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ *
+ */
+class Sample
+    {
+    protected $value;
+
+    /**
+     * Sample function Doc Comment
+     *
+     * @category Function
+     * @package  sample
+     * @author    A N Other 
+     * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+     * @link     http://www.hashbangcode.com/
+     *
+     */
+    public function getValue()
+    {
+        return $this->value;
+        throw new RuntimeException('Not yet implemented.');
+    }
+
+    /**
+     * Sample function Doc Comment
+     *
+     * @category Function
+     * @package  sample
+     * @author    A N Other 
+     * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+     * @link     http://www.hashbangcode.com/
+     *
+     */
+    public function setValue($value)
+    {
+        $this->value = $value * 2;
+    }
 } 
 ?>
